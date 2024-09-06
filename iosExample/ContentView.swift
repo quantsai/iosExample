d8e610267777d02883fe59bec84a81618e213532
caiquan <396022671@qq.com>
2024-09-06 13:27:34 +0800

    Initial Commit

---
+++ iosExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iosExample
+//
+//  Created by 蔡铨 on 2024/9/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iosExample/iosExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosExampleApp.swift
+//  iosExample
+//
+//  Created by 蔡铨 on 2024/9/6.
+//
+
+import SwiftUI
+
+@main
+struct iosExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
